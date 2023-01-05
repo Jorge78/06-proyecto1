a99b73b8258852b0fb0d84b5459cd05b22ee4fad
janr78 <jan.rock78@gmail.com>
2023-01-04 20:42:05 -0600

    Atualizacion del listado

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -62,37 +62,7 @@ export const Listado = ({listadoState, setListadoState}) => {
                 
             }
 
-            {/* <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article> */}
+            
         </>
     )
 }
